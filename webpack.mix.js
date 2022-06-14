83a89b741c302e35643adb5194059de46f7f1f48
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2022-06-14 17:57:25 -0400

    [FEATURE] SCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,35 @@
 // webpack.mix.js
-let mix = require("laravel-mix");
+const mix = require("laravel-mix");
 require("laravel-mix-twig");
 
 mix
+  .setPublicPath("dist/")
   .twig({
+    enabled: !mix.inProduction(),
     root: "src/views/",
     output: ".",
+    beautify: {
+      indent_inner_html: true,
+    },
   })
-  .js("src/js/app.js", "dist")
-  .setPublicPath("dist")
+  .sass("src/scss/app.scss", "css")
+  .js("src/js/app.js", "js")
   .browserSync({
-    server: "dist/",
+    server: "dist",
+    watch: ["*.html"],
   });
+
+// HACK: 1
+// mix.webpackConfig({
+//   output: {
+//     publicPath: "",
+//   },
+// });
+
+// HACK: 2
+// mix.override((config) => {
+//   config.entry = Object.keys(config.entry).reduce((acc, key) => {
+//     acc[key.replace(/^\//, "")] = config.entry[key];
+//     return acc;
+//   }, {});
+// });
